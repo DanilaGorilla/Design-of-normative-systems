7982ad6da47f3e4a03d06eba36f275f138cbb0e0
DanilaGorilla <131073966+DanilaGorilla@users.noreply.github.com>
2025-09-13 10:38:01 +0300

    main для 6-го .java

--- Laba1/main.java
+++ Laba1/main.java
@@ -1,7 +1,10 @@
+package org.example;
 import java.time.LocalDate;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static <Client> void main(String[] args) {
+        //Обычный
         try {
             Delivery delivery1 = new Delivery(
                     1,
@@ -10,20 +13,51 @@ public class Main {
                     LocalDate.of(2025, 9, 12),
                     LocalDate.of(2025, 9, 15)
             );
-            System.out.println("Успешно создан Delivery:");
-            System.out.println("deliveryId = " + delivery1.getDeliveryId());
-            System.out.println("routeId = " + delivery1.getRouteId());
-            System.out.println("driverId = " + delivery1.getDriverId());
-            System.out.println("departureDate = " + delivery1.getDepartureDate());
-            System.out.println("arrivalDate = " + delivery1.getArrivalDate());
+            System.out.println("Успешно создан Delivery через конструктор:");
+            printDelivery(delivery1);
         } catch (IllegalArgumentException e) {
             System.out.println("Ошибка при создании Delivery: " + e.getMessage());
         }
 
-        System.out.println("\n---------------------------\n");
-
+        //CSV
         try {
-            Delivery delivery2 = new Delivery(
+            String csv = "2,20,200,2025-10-01,2025-10-05";
+            Delivery delivery2 = new Delivery(csv);
+            System.out.println("Успешно создан Delivery через CSV строку:");
+            printDelivery(delivery2);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка при создании Delivery из CSV: " + e.getMessage());
+        }
+
+
+        //JSON
+        try {
+            Delivery delivery4 = new Delivery("delivery.json", true);
+            System.out.println("Успешно создан Delivery через JSON файл:");
+            printDelivery(delivery4);
+        } catch (Exception e) {
+            System.out.println("Ошибка при создании Delivery из JSON файла: " + e.getMessage());
+        }
+        
+        try {
+            String jsonFilePath = "C:/Users/user/Desktop/123.json";
+
+            Delivery delivery = new Delivery(jsonFilePath, true);
+
+            System.out.println("deliveryId = " + delivery.getDeliveryId());
+            System.out.println("routeId = " + delivery.getRouteId());
+            System.out.println("driverId = " + delivery.getDriverId());
+            System.out.println("departureDate = " + delivery.getDepartureDate());
+            System.out.println("arrivalDate = " + delivery.getArrivalDate());
+
+        } catch (Exception e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        //ошибки
+        try {
+            Delivery delivery5 = new Delivery(
                     -5,
                     10,
                     100,
@@ -31,21 +65,26 @@ public class Main {
                     LocalDate.of(2025, 9, 15)
             );
         } catch (IllegalArgumentException e) {
-            System.out.println("Ожидаемая ошибка: " + e.getMessage());
+            System.out.println("Ожидаемая ошибка (deliveryId): " + e.getMessage());
         }
 
-        System.out.println("\n---------------------------\n");
-
         try {
-            Delivery delivery3 = new Delivery(
-                    2,
+            Delivery delivery6 = new Delivery(
+                    6,
                     11,
                     101,
                     LocalDate.of(2025, 9, 15),
                     LocalDate.of(2025, 9, 12)
             );
         } catch (IllegalArgumentException e) {
-            System.out.println("Ожидаемая ошибка: " + e.getMessage());
+            System.out.println("Ожидаемая ошибка (arrivalDate): " + e.getMessage());
         }
+    }
+    private static void printDelivery(Delivery delivery) {
+        System.out.println("deliveryId = " + delivery.getDeliveryId());
+        System.out.println("routeId = " + delivery.getRouteId());
+        System.out.println("driverId = " + delivery.getDriverId());
+        System.out.println("departureDate = " + delivery.getDepartureDate());
+        System.out.println("arrivalDate = " + delivery.getArrivalDate());
     }
 }
